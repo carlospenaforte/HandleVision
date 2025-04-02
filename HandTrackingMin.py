dd72c352467181b9ba6b82a13e85e3f53e6b5e22
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-02 14:55:11 -0300

    Add POINTS TO HANDS AND FPS
    ADD FPS TO SCREAM
    ADD COLORS TO THE POINTS IN THE HANDS
    ADD CONFIG TO CAPT 2 HANDS

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,13 +5,32 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 
 while True:
     ret, frame = cap.read()
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(frameRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+            
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame, str(int(fps)), (19,70), cv2.FONT_ITALIC, 3, (255,0,0), 3)
 
     cv2.imshow("frame", frame)
-
     if cv2.waitKey(1) == ord('q'):
         break
 
