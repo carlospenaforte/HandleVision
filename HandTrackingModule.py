93d111ac99b248502fc00f3ba93d6dcc5be61ff8
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-03 19:37:03 -0300

    activate for loop for position

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -28,13 +28,13 @@ class handDetector():
 
         def findPosition(self, frame, handNo=0, draw=True):
 
-                # for id, lm in enumerate(handLms.landmark):
-                #    # print(id, lm)
-                #    h, w, c = frame.shape
-                #    cx, cy = int(lm.x * w), int(lm.y * h)
-                #    print(id, cx, cy)
-                #    # if id == 4:
-                #    cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                for id, lm in enumerate(handLms.landmark):
+                  # print(id, lm)
+                    h, w, c = frame.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    print(id, cx, cy)
+                    # if id == 4:
+                    cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
 
 def main():
     pTime = 0
