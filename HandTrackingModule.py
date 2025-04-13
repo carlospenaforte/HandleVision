5034f14a423cc271ca0523023034e80b34a4c02d
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-13 13:12:57 -0300

    module for hand position

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -18,23 +18,29 @@ class handDetector():
 
     def findHands(self, frame, draw=True):
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(frameRGB)
+        self.results = self.hands.process(frameRGB)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
         return frame
 
         def findPosition(self, frame, handNo=0, draw=True):
 
-                for id, lm in enumerate(handLms.landmark):
-                  # print(id, lm)
-                    h, w, c = frame.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    print(id, cx, cy)
-                    # if id == 4:
-                    cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                lmList = []
+                if self.results.multi_hand_landmarks:
+                    myHand = self.results.multi_hand_landmarks[handNo]
+                    for id, lm in enumerate(myHand.landmark):
+                        # print(id, lm)
+                        h, w, c = frame.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        # print(id, cx, cy)
+                        lmList.append([id, cx, cy])
+                        if draw:
+                            cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+                return lmList
 
 def main():
     pTime = 0
@@ -44,6 +50,9 @@ def main():
     while True:
         ret, frame = cap.read()
         frame = detector.findHands(frame)
+        lmList = detector.findPosition(frame)
+        if len(lmList) != 0:
+            print(lmList[0])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
